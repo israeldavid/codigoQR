cb4284f40469dfe04a8290664c7ab7dc4a68ee3c
David <israeldavid@gmail.com>
2020-01-18 23:28:32 -0500

    primer Commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { Base64ToGallery } from '@ionic-native/base64-to-gallery/ngx';
+import { ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +10,35 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  qrData='http://www.quehacedavid.com';
+  scannedCode=null;
+  elementType:'url'|'canvas'|'img' = 'canvas';
+  
+
+  constructor(private barcodeScanner:BarcodeScanner,private base64ToGallery:Base64ToGallery,private toastCtrl:ToastController) {}
+
+  scanCode(){
+    this.barcodeScanner.scan().then(
+      barcodeData => {
+        this.scannedCode = barcodeData.text;
+      }
+    )
+  }
+
+  downloadQr(){
+    const canvas = document.querySelector('canvas') as HTMLCanvasElement;
+    const imageData = canvas.toDataURL('image/jpeg').toString();
+    console.log('data: ',imageData);
+
+    let data = imageData.split(',')[1];
+
+    this.base64ToGallery.base64ToGallery(data, 
+      {prefix: '_img',mediaScanner:true})
+      .then(async res => {
+        let toast = await this.toastCtrl.create({
+          header: 'QR Code grabado en la libreria'
+        })
+      }, err=> console.log('err: ',err ));
+  }
 
 }
